ce3ed87d6ac25760386d8af80118ed20322c6f8f
Boban Mihailov <matoriinfos@gmail.com>
2017-12-15 08:24:32 +0100

    Inicijalni commit

---
+++ app/src/main/java/ser_comm/vs_pc/retrofitandro/MainActivity.java
@@ -0,0 +1,13 @@
+package ser_comm.vs_pc.retrofitandro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
