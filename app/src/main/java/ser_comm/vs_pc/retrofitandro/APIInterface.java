53958c01650cebcf3da559c2a7026306b49b9360
matoriinfos <matoriinfos@gmail.com>
2017-12-18 11:50:22 +0100

    Generisane POJO klase za JSON response i kreiran API interfejs za HTTP zahteve

--- app/src/main/java/ser_comm/vs_pc/retrofitandro/APIInterface.java
+++ app/src/main/java/ser_comm/vs_pc/retrofitandro/APIInterface.java
@@ -1,10 +1,32 @@
 package ser_comm.vs_pc.retrofitandro;
 
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+import ser_comm.vs_pc.retrofitandro.pojo.MultipleResource;
+import ser_comm.vs_pc.retrofitandro.pojo.User;
+import ser_comm.vs_pc.retrofitandro.pojo.UserList;
+
 /**
  * Created by VS-PC on 15.12.2017..
  */
 
 public interface APIInterface {
 
+    @GET("/api/unknown")
+    Call<MultipleResource>doGetListResources();
 
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page")String page);
+
+    @FormUrlEncoded
+    @POST("/api/users")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
 }
